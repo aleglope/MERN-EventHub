22c6cb9d168596850b589b5bd38f0765ad652682
AGonzL <agonzlopez.11@gmail.com>
2024-06-27 03:12:12 +0200

    try to fix bug

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,41 +1,41 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
-import { WebhookEvent, clerkClient } from '@clerk/nextjs/server'
-import { create } from 'domain'
+import { WebhookEvent } from '@clerk/nextjs/server'
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
+import { clerkClient } from '@clerk/clerk-sdk-node'
 import { NextResponse } from 'next/server'
-
+ 
 export async function POST(req: Request) {
-
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
+ 
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
-
+ 
   if (!WEBHOOK_SECRET) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
-
+ 
   // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
-
+ 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response('Error occured -- no svix headers', {
       status: 400
     })
   }
-
+ 
   // Get the body
   const payload = await req.json()
   const body = JSON.stringify(payload);
-
+ 
   // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
-
+ 
   let evt: WebhookEvent
-
+ 
   // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
@@ -49,39 +49,42 @@ export async function POST(req: Request) {
       status: 400
     })
   }
-
-  // Do something with the payload
-  // For this guide, you simply log the payload to the console
+ 
+  // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
+ 
   if(eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
       clerkId: id,
-      email: email_addresses[0],
+      email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name!,
+      lastName: last_name!,
       photo: image_url,
     }
-    
+
     const newUser = await createUser(user);
+
     if(newUser) {
-        await clerkClient.users.updateUserMetadata(id, {
-          publicMetadata: {
-            userId: newUser._id,
-          }
-    })
-}
-    return NextResponse.json({ message: 'OK', user: newUser})
-}
-if (eventType === 'user.updated') {
+      await clerkClient.users.updateUserMetadata(id, {
+        publicMetadata: {
+          userId: newUser._id
+        }
+      })
+    }
+
+    return NextResponse.json({ message: 'OK', user: newUser })
+  }
+
+  if (eventType === 'user.updated') {
     const {id, image_url, first_name, last_name, username } = evt.data
 
     const user = {
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name!,
+      lastName: last_name!,
       username: username!,
       photo: image_url,
     }
