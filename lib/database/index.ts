1826cdac517ae4ff3c17b4c983bbb49482fc3640
AGonzL <agonzlopez.11@gmail.com>
2024-06-27 17:57:51 +0200

    .

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -4,17 +4,34 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
 export const connectToDatabase = async () => {
-  if (cached.conn) return cached.conn;
+  if (cached.conn) {
+    console.log('Using cached MongoDB connection');
+    return cached.conn;
+  }
 
-  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+  if (!MONGODB_URI) {
+    throw new Error('MONGODB_URI is missing');
+  }
 
-  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    dbName: 'Groove',
-    bufferCommands: false,
-  })
+  if (!cached.promise) {
+    console.log('Connecting to MongoDB');
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      dbName: 'Groove',
+      bufferCommands: false,
+    }).then((mongoose) => {
+      console.log('Connected to MongoDB');
+      return mongoose;
+    }).catch((err) => {
+      console.error('Error connecting to MongoDB:', err);
+      throw err;
+    });
+  }
 
   cached.conn = await cached.promise;
-
   return cached.conn;
-}
+};
